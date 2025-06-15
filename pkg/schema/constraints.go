ada96e5c44602e9d701b6e0c396816a60436db74
someoccasion <150419864+someoccasion@users.noreply.github.com>
2025-06-15 23:28:35 +0400

    refactor: move single-constraint building function into constraints package

--- pkg/schema/constraints.go
+++ pkg/schema/constraints.go
@@ -8,33 +8,21 @@ import (
 	"github.com/hydridity/Schematic/pkg/schema/context"
 )
 
-type constraintBuilder = func(part *parser.Part) context.Constraint
-
-func getConstraintBuilders() []constraintBuilder {
-	return []constraintBuilder{
-		func(part *parser.Part) context.Constraint { return constraints.TryCreateLiteralConstraint(part) },
-		func(part *parser.Part) context.Constraint { return constraints.TryCreateVariableConstraint(part) },
-		func(part *parser.Part) context.Constraint { return constraints.TryCreateVariableSetConstraint(part) },
-		func(part *parser.Part) context.Constraint { return constraints.TryCreateWildcardMultiConstraint(part) },
-		func(part *parser.Part) context.Constraint { return constraints.TryCreateWildcardSingleConstraint(part) },
-	}
+// The basic interface of our constraints.
+type Constraint interface {
+	Consume([]string, *ValidationContext) ([]string, error)
+	String() string
+	GetVariableName() string
 }
 
-func CompileConstraints(schemaAst *parser.SchemaAST) []context.Constraint {
-	res := make([]context.Constraint, 0, len(schemaAst.Parts))
-	builders := getConstraintBuilders()
-
+func CompileConstraints(schemaAst *parser.SchemaAST) []Constraint {
+	res := make([]Constraint, 0, len(schemaAst.Parts))
 	for _, part := range schemaAst.Parts {
-		var constraint context.Constraint = nil
-		for _, builder := range builders {
-			constraint = builder(part)
-			if constraint != nil {
-				break
-			}
+		constraint, err := constraints.CreateConstraint(part)
+		if err != nil {
+			log.Printf("Error creating constraint: %v", err)
 		}
-		if constraint == nil {
-			log.Fatalf("Failed to compile constraints: cannot translate AST part %s", part.String())
-		}
+		res = append(res, constraint)
 	}
 
 	return res
---
+++ pkg/schema/constraints/constraints.go
@@ -0,0 +1,32 @@
+package constraints
+
+import (
+	"fmt"
+	"github.com/hydridity/Schematic/pkg/parser"
+	"github.com/hydridity/Schematic/pkg/schema"
+	"log"
+)
+
+type constraintBuilder = func(part *parser.Part) schema.Constraint
+
+var constraintBuilders = []constraintBuilder{
+	func(part *parser.Part) schema.Constraint { return TryCreateLiteralConstraint(part) },
+	func(part *parser.Part) schema.Constraint { return TryCreateVariableConstraint(part) },
+	func(part *parser.Part) schema.Constraint { return TryCreateVariableSetConstraint(part) },
+	func(part *parser.Part) schema.Constraint { return TryCreateWildcardMultiConstraint(part) },
+	func(part *parser.Part) schema.Constraint { return TryCreateWildcardSingleConstraint(part) },
+}
+
+func CreateConstraint(part *parser.Part) (schema.Constraint, error) {
+	var constraint schema.Constraint = nil
+	for _, builder := range constraintBuilders {
+		constraint = builder(part)
+		if constraint != nil {
+			break
+		}
+	}
+	if constraint == nil {
+		return nil, fmt.Errorf("failed to compile constraints: cannot translate AST part %s", part.String())
+	}
+	return constraint, nil
+}
