7ac484d9a3a0d4c8149f2befbb055fc9c666d760
hydridity <hydridity@nyara.network>
2025-06-25 19:53:56 +0200

    feat: consume subschemas

--- pkg/schema/constraints.go
+++ pkg/schema/constraints.go
@@ -158,24 +158,40 @@ func (c *VariableSetConstraint) Consume(path []string, context *ValidationContex
 	if len(path) <= 0 {
 		return nil, errors.New("empty path")
 	}
+
 	variable, found := context.VariableStore.GetVariableSet(c.VariableName)
 	if !found {
 		return nil, fmt.Errorf("variable '%s' not found in store", c.VariableName)
 	}
+
 	if len(variable) == 0 {
 		return nil, fmt.Errorf("variable set '%s' is empty", c.VariableName)
 	}
+
 	foundInSet := false
 	for _, v := range variable {
-		if path[0] == v {
+		subSchemaInput := make([]string, len(path))
+		copy(subSchemaInput, path)
+		subSchemaCompiled, err := CreateSchema(v)
+		if err != nil {
+			return nil, err
+		}
+
+		subSchemaInput, err = subSchemaCompiled.consume(subSchemaInput, context)
+
+		if err == nil {
+			fmt.Println("Validated?")
 			foundInSet = true
+			path = subSchemaInput
 			break
 		}
 	}
+
 	if !foundInSet {
 		return nil, errors.New("invalid variable set constraint value")
 	}
-	return path[1:], nil
+
+	return path, nil
 }
 
 func (c *VariableSetConstraint) String() string {
